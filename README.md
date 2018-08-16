3309f52f682b6a548aacd64e7442a98f2a9abfd1
György Novák <gyorgy.novak@loxxly.com>
2018-08-16 05:08:12 +0200

    Adding ngx_cache_purge module

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is the Git repo of the Docker image for [nginx-pagespeed](https://hub.docker.com/r/hitko/nginx-pagespeed/), based off the official nginx image. See the
 [Nginx Hub page](https://registry.hub.docker.com/_/nginx/) for the full readme on how to use the Docker image and for information
 regarding contributing and issues.
+
+Our goloxxly fork adds [ngx_cache_purge module](https://github.com/FRiCKLE/ngx_cache_purge/) to the [Liquid-JS version](https://github.com/Liquid-JS/nginx-pagespeed-docker).
--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ apt-get source nginx=${NGINX_VERSION}-1
 mkdir -p nginx-${NGINX_VERSION}/debian/modules
 cd nginx-${NGINX_VERSION}/debian/modules
 
+wget http://labs.frickle.com/files/ngx_cache_purge-2.3.tar.gz -O ngx_cache_purge-2.3.tar.gz
+tar -xzvf ngx_cache_purge-2.3.tar.gz
+
 wget https://github.com/apache/incubator-pagespeed-ngx/archive/v${NPS_VERSION}.zip -O release-${NPS_VERSION}.zip
 unzip release-${NPS_VERSION}.zip
 cd incubator-pagespeed-ngx-${NPS_VERSION}/
@@ -16,7 +19,7 @@ tar -xzvf $(basename ${psol_url})
 cd /nginx/nginx-${NGINX_VERSION}
 wget https://www.openssl.org/source/openssl-${OSSL_VERSION}.tar.gz
 tar -xf openssl-${OSSL_VERSION}.tar.gz
-sed -i "0,/CFLAGS\=\\\"\\\"/{/CFLAGS\=\\\"\\\"/ s/$/ --add-module=\/nginx\/nginx-${NGINX_VERSION}\/debian\/modules\/incubator-pagespeed-ngx-${NPS_VERSION} ${PS_NGX_EXTRA_FLAGS} --with-openssl=\/nginx\/nginx-${NGINX_VERSION}\/openssl-${OSSL_VERSION}/}" /nginx/nginx-${NGINX_VERSION}/debian/rules
+sed -i "0,/CFLAGS\=\\\"\\\"/{/CFLAGS\=\\\"\\\"/ s/$/ --add-module=\/nginx\/nginx-${NGINX_VERSION}\/debian\/modules\/ngx_cache_purge-2.3 --add-module=\/nginx\/nginx-${NGINX_VERSION}\/debian\/modules\/incubator-pagespeed-ngx-${NPS_VERSION} ${PS_NGX_EXTRA_FLAGS} --with-openssl=\/nginx\/nginx-${NGINX_VERSION}\/openssl-${OSSL_VERSION}/}" /nginx/nginx-${NGINX_VERSION}/debian/rules
 #sed "41 a --add-module=/nginx/nginx-${NGINX_VERSION}/debian/modules/ngx_pagespeed-release-${NPS_VERSION} ${PS_NGX_EXTRA_FLAGS}" -i /nginx/nginx-${NGINX_VERSION}/debian/rules
 #sed "46 a --with-openssl=/nginx/nginx-${NGINX_VERSION}/openssl-${OSSL_VERSION}" -i /nginx/nginx-${NGINX_VERSION}/debian/rules
 
